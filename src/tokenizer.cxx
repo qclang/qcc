2ae5a532299f7cd72f619e221e7a8c0fa051ab88
lahit1 <naifcanbasci@gmail.com>
2025-07-27 19:43:48 +0000

    Add index, line and line_beg, change miss-defitions(referenced ones), integrate to previously edited header file

--- src/tokenizer.cxx
+++ src/tokenizer.cxx
@@ -9,21 +9,27 @@ namespace Tokenizer {
 
 	std::vector<Token> tokens;
 
+	size_t index, line, line_beg;
+
 void proc(char *data, size_t length) {
-	size_t index=0;
+	index = line_beg = 0;
+	line = 1;
 	char curr, peek;
 
         while(index < length) {
 		curr = data[index];
 		peek = data[index + 1];
                 if((curr <= 'Z' && curr >= 'A') || ('a' <= curr && curr <= 'z')) { // Identifiers/keyword must start with an alphabetic char
-			Token& idtf = readAlpNum(data, index);
+			Token idtf = readAlpNum(data, index);
 		} else if('0' <= curr && curr <= '9') { // Number literal
-			Token& num = readNum(data, index);
+			Token num = readNum(data, index);
 		} else if(curr == '\'') { // Character literal
-			Token& c = readChar(data, index);
+			Token c = readChar(data, index);
 		} else if(curr == '"') { // String literal
-			Token& s = readString(data, index);
+			Token s = readString(data, index);
+		} else if(curr == '\n') {
+			++line;
+			line_beg = ++index;
 		} else if(curr == '\0') break;
         }
 }
@@ -45,8 +51,15 @@ Token readAlpNum(char* data, size_t& index) {
 	std::string ret =  buff;
 	delete[] buff;
 
+	Token tok;
+	tok.name = ret;
+	tok.line = line;
+	tok.column = end - line_beg + 1;
+	tok.startOffset = index;
+	tok.endOffset = end;
+
 	index=end;
-	return ret;
+	return tok;
 }
 
 Token readNum(char* data, size_t& index) {
@@ -60,28 +73,51 @@ Token readNum(char* data, size_t& index) {
 	buff[len] = '\0';
         std::string ret = buff;
 	delete[] buff;
+
+	Token tok;
+	tok.name = ret;
+        tok.line = line;
+        tok.column = end - line_beg + 1;
+        tok.startOffset = index;
+        tok.endOffset = end;
+
+
 	index=end;
-	return ret;
+	return tok;
 }
 
 Token readChar(char* data, size_t& index) {
-	++index;
-	if(data[index] == '\\') {
-		switch(++index) {
+	size_t end = ++index;
+	char c;
+	if(data[end] == '\\') {
+		switch(++end) {
 			case 'n':
-				return '\n';
+				c =  '\n';
+				break;
 			case 't':
-				return '\t';
+				c = '\t';
+				break;
 			case '0':
-				return '\0';
+				c = '\0';
+				break;
 			case '\\':
-				return '\\';
+				c = '\\';
+				break;
 			case '\'':
-                                return '\'';
+                                c = '\'';
+				break;
 		}
-	}
-	return data[index++];
+	} else c = data[end];
 
+	Token tok;
+	tok.name = c;
+        tok.line = line;
+        tok.column = end - line_beg + 1;
+        tok.startOffset = index;
+        tok.endOffset = end;
+
+	index=end;
+	return tok;
 }
 
 Token readString(char* data, size_t& index) {
@@ -128,8 +164,18 @@ Token readString(char* data, size_t& index) {
 	buff[len] = '\0';
         std::string ret =  buff;
 	delete[] buff;
+
+
+	Token tok;
+	tok.name = ret;
+        tok.line = line;
+        tok.column = end - line_beg + 1;
+        tok.startOffset = index;
+        tok.endOffset = end;
+
+
 	index=end;
-	return ret;
+	return tok;
 }
 
 }
