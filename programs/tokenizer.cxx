a4cd579c5d76467299ec1a83ee8438ce3160e83c
Naifcan Başçı <naifcanbasci@gmail.com>
2025-08-04 17:50:17 +0300

    remove -F option, 	if you want to proccess a file give the file-path directly 	usage: 'tokenizer <filepath>'
    add -o option,
    	select destination file path to write output
    	usage: 'tokenizer -o output.token'
    
    	notice!: if you won't specify input file path, program will read input from console defaultly
    	notice!: if you won't specify output file path with -o option, program will print output to console defaultly

--- programs/tokenizer.cxx
+++ programs/tokenizer.cxx
@@ -5,35 +5,50 @@
 
 int main(int argc, char* argv[]) {
 	std::istream* in = nullptr;
+	std::ostream* out = nullptr;
 
 	for(int i=1; i < argc; i++) {
 		std::string arg = argv[i];
-		if(arg == "-F") 
+		if(arg == "-o")
 			if(++i < argc) {
 				arg = argv[i];
 				if(arg != "-") {
-					std::ifstream *fin = new std::ifstream(arg);
-					if(!fin->is_open()) {
+					std::ofstream *fout = new std::ofstream(arg);
+					if(!fout->is_open()) {
 						std::cerr << "Can't open the file: " << arg << std::endl;
-						delete fin;
+						delete fout;
 						return 1;
 					}
-					in = fin;
+					out = fout;
 				}
 			} else {
-				std::cerr << "File path not given after '-F' option" << std::endl;
+				std::cerr << "File path not given after '-o' option" << std::endl;
 				return 1;
 			}
+		else {
+                        std::ifstream *fin = new std::ifstream(arg);
+                        if(!fin->is_open()) {
+                                std::cerr << "Can't open the file: " << arg << std::endl;
+                                delete fin;
+                                return 1;
+                        }
+                        in = fin;
+                }
 	}
 
 	if(in == nullptr)
 		in = &std::cin;
 
+	if(out == nullptr)
+		out = &std::cout;
+
 	Tokenizer::use(*in);
-	Tokenizer::use(std::cout);
+	Tokenizer::use(*out);
 
 	Tokenizer::proc();
 
 	if(in != &std::cin)
 		delete in;
+	if(out != &std::cout)
+		delete out;
 };
