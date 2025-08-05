15d0850bf39f4e3d3ceb03506b168eedf2f67af8
Naifcan Başçı <naifcanbasci@gmail.com>
2025-08-05 16:00:00 +0300

    Update build script

--- build.sh
+++ build.sh
@@ -10,13 +10,12 @@ std=-std=c++17
 arg="$1"
 
 if [[ "$arg" == "tok" || -z $arg ]]; then
-        # Dosyaları diziye güvenli şekilde ekle
         srcs=(programs/tokenizer.cxx)
         while IFS= read -r file; do
                 srcs+=("$file")
         done < <(find src/tokenizer/ -type f -regex '.*\.\(cxx\|cpp\)$')
 
-        echo "Source files:"
+        echo "Tokenizer source files:"
         printf '  %s\n' "${srcs[@]}"
 
         objs=()
@@ -33,4 +32,29 @@ if [[ "$arg" == "tok" || -z $arg ]]; then
         echo "🔗 Linking -> bin/tokenizer"
         g++ $std -I./include "${objs[@]}" -o bin/tokenizer
         echo "✅ Build success: ./bin/tokenizer"
+fi
+
+if [[ "$arg" == "par" || -z $arg ]]; then
+        srcs=(programs/tokenparser.cxx)
+        while IFS= read -r file; do
+                srcs+=("$file")
+        done < <(find src/tokenparser/ src/tokenizer/tokens.cxx -type f -regex '.*\.\(cxx\|cpp\)$')
+
+        echo "Tokenparser source files:"
+        printf '  %s\n' "${srcs[@]}"
+
+        objs=()
+        for src in "${srcs[@]}"; do
+                obj="./build/$(echo "$src" | tr "/" "_").o"
+                echo "🔧 Compiling $src -> $obj"
+                g++ $std -march=native -c -I./include "$src" -o "$obj"
+                objs+=("$obj")
+        done
+
+        echo "Object files:"
+        printf '  %s\n' "${objs[@]}"
+
+        echo "🔗 Linking -> bin/tokenparser"
+        g++ $std -I./include "${objs[@]}" -o bin/tokenparser
+        echo "✅ Build success: ./bin/tokenparser"
 fi
