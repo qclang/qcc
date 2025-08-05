1e8917cabc07268bbccd7f931f700b81ee2a2d8c
Naifcan Başçı <naifcanbasci@gmail.com>
2025-08-05 15:55:10 +0300

    Add tokenparser main program

---
+++ programs/tokenparser.cxx
@@ -0,0 +1,54 @@
+#include <tokenparser/tokenparser.hxx>
+#include <iostream>
+#include <fstream>
+#include <string>
+
+int main(int argc, char* argv[]) {
+	std::istream* in = nullptr;
+	std::ostream* out = nullptr;
+
+	for(int i=1; i < argc; i++) {
+		std::string arg = argv[i];
+		if(arg == "-o")
+			if(++i < argc) {
+				arg = argv[i];
+				if(arg != "-") {
+					std::ofstream *fout = new std::ofstream(arg);
+					if(!fout->is_open()) {
+						std::cerr << "Can't open the file: " << arg << std::endl;
+						delete fout;
+						return 1;
+					}
+					out = fout;
+				}
+			} else {
+				std::cerr << "File path not given after '-o' option" << std::endl;
+				return 1;
+			}
+		else {
+                        std::ifstream *fin = new std::ifstream(arg);
+                        if(!fin->is_open()) {
+                                std::cerr << "Can't open the file: " << arg << std::endl;
+                                delete fin;
+                                return 1;
+                        }
+                        in = fin;
+                }
+	}
+
+	if(in == nullptr)
+		in = &std::cin;
+
+	if(out == nullptr)
+		out = &std::cout;
+
+	Tokenparser::use(*in);
+	Tokenparser::use(*out);
+
+	Tokenparser::proc();
+
+	if(in != &std::cin)
+		delete in;
+	if(out != &std::cout)
+		delete out;
+};
