ae8b2a7aef1c9436ec3c1cb2dcc3347d8aabbc5a
Naifcan Başçı <naifcanbasci@gmail.com>
2025-08-05 15:18:50 +0300

    Remove unused/deprecated class/struct member

--- include/tokenizer/tokenizer.hxx
+++ include/tokenizer/tokenizer.hxx
@@ -1,13 +1,9 @@
 #pragma once
 
-#include<unordered_map>
-#include<vector>
 #include<istream>
 #include<tokenizer/tokens.hxx>
 
 namespace Tokenizer {
-
-	extern std::vector<Token> tokens;
 
 	void use(std::istream &input_stream);
 	void use(std::ostream &output_stream);
