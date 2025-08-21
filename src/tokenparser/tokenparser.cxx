d81718e5dd8687a52a96fcd314da512002f51761
lahit1 <naifcanbasci@gmail.com>
2025-08-21 13:28:21 +0300

    Support variable declaration but not definition

--- src/tokenparser/tokenparser.cxx
+++ src/tokenparser/tokenparser.cxx
@@ -19,12 +19,13 @@ namespace Tokenparser {
 
 	int eat(Tokens::Type ttype) { // return 1 if match, 0 if not match
 		if(c_token.ttype == ttype) {
+			std::cout << "Eat: " << c_token.line << ":" << c_token.name << std::endl;
 			_input_stream >> c_token;
 			return 1;
 		}
 
 		return 0;
-	};
+	}
 
 	uint8_t getPVarT(const std::string& str) {
 		if(str == "angle")
@@ -43,16 +44,16 @@ namespace Tokenparser {
 			return VAR_DOUBLE;
 		else if(str == "long")
 			return VAR_LONG;
+		return VAR_UNDEFINED;
 	}
 
-	int eatTyper(std::shared_ptr<Typer>& c_typer) {
-		c_typer = std::make_shared<Typer>();
-		c_typer->vtype = VAR_UNDEFINED;
-		c_typer->spec = char(0);
+	ExprPtr eval(Tokens::Type till);
+
+	int eatTyper(std::shared_ptr<Typer>& c_typer, bool followAll, std::vector<StmPtr>* parent = nullptr) {
 		while(1) {
-			std::string _name=c_token.name;
-			if(eat(Tokens::TOK_TYPE)) {
-				uint8_t vtype = getPVarT(_name);
+			if(c_token.ttype == Tokens::TOK_TYPE) {
+				uint8_t vtype = getPVarT(c_token.name);
+				eat(Tokens::TOK_TYPE);
                                 if(vtype == VAR_LONG) switch(c_typer->vtype) {
 					case VAR_LONG:
 					case VAR_DOUBLE:
@@ -75,12 +76,16 @@ namespace Tokenparser {
 				c_typer->spec |= (1 << SPEC_EXT);
 			else if(eat(Tokens::TOK_KEY_VOLATILE))
 				c_typer->spec |= (1 << SPEC_VOL);
-			else if(eat(Tokens::TOK_STAR)) {
+			else if(followAll && eat(Tokens::TOK_STAR)) {
 				std::shared_ptr<Typer> ptr_typer = std::make_shared<Typer>();
 				ptr_typer->vtype = VAR_POINTER;
-				ptr_typer->respect_ptr = c_typer;
+				ptr_typer->respect_typer = c_typer;
 				c_typer = ptr_typer;
-			} else break;
+			} else if(parent && c_token.ttype == Tokens::TOK_IDENTIFIER) {
+				std::cout << "Dec: " << c_token.name << std::endl;
+				eat(Tokens::TOK_IDENTIFIER);
+				return 1;
+                        } else break;
 		};
 		return c_typer->vtype;
 	};
@@ -93,21 +98,34 @@ namespace Tokenparser {
 		return nullptr;
 	} // eval array sizer or indexer
 
-	void evalDec() {} // Return declarationstatement
+
+	int eatDec(std::vector<StmPtr>& parent) {
+		std::shared_ptr<Typer> main_typer = std::make_shared<Typer>();
+		if(!eatTyper(main_typer, false))
+			return 0;
+		do {
+			std::shared_ptr<Typer> c_typer = std::make_shared<Typer>(*main_typer);
+			if(!eatTyper(c_typer, true, &parent)) {
+				//error
+				return 0;
+			}
+		} while(eat(Tokens::TOK_COMMA));
+		return 1;
+	}
 
 	StmPtr evalFunc() {
 		return nullptr;
 	}
 
 	int proc(std::vector<StmPtr> &parent) {
-		while(_input_stream && _input_stream->peek() != EOF) {
-			_input_stream >> c_token;
+		if(!_input_stream) return 1;
+		_input_stream >> c_token;
+		while(!eat(Tokens::TOK_SYS_EOF)) {
 			std::string _name = c_token.name;
 
 			std::shared_ptr<Typer> m_typer;
-			if(eatTyper(m_typer)) {
-				//eatIdtf or throw error
-			}
+			if(eatDec(parent)) continue;
+			
 		}
 
 		return 0;
