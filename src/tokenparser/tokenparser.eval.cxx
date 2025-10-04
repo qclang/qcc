3261bf1da636db1c515a78e9d444a91f32f41744
lahit1 <naifcanbasci@gmail.com>
2025-10-04 11:10:27 +0300

    Simple number and variable evaluation

--- src/tokenparser/tokenparser.eval.cxx
+++ src/tokenparser/tokenparser.eval.cxx
@@ -1,16 +1,28 @@
 #include <tokenparser/tokenparser.hxx>
+#include <tokenparser/expressions.hxx>
 
 namespace Tokenparser {
 
 
 	extern Token c_token;
 
-
         ExprPtr eval(Tokens::Type till) {
-		if(eat(Tokens::TOK_IDENTIFIER)) {
-			
+		if(c_token.ttype == Tokens::TOK_IDENTIFIER) {
+			std::string name = c_token.name;
+			eat(Tokens::TOK_IDENTIFIER);
+			if(eat(Tokens::TOK_ASSIGN))
+				return std::make_shared<BinaryExpression>(
+							std::make_shared<VariableExpression>(name),
+							OPE::ASSIGN,
+							eval(Tokens::TOK_SEMICOLON));
+		} else if(c_token.ttype == Tokens::TOK_NUMBER_LITERAL) {
+			std::string name = c_token.name;
+			eat(Tokens::TOK_NUMBER_LITERAL);
+
+			return std::make_shared<LiteralExpression>(LIT_NUMBER, name);
 		}
-			
+
+		return 0;
 	}
 
 }
