8a1444f4436f3dd442cc0122c43d07c7908cff77
lahit1 <naifcanbasci@gmail.com>
2025-10-06 17:19:01 +0300

    Add assignment operator and post/pre inncrement/decrement operators

--- src/tokenparser/tokenparser.eval.cxx
+++ src/tokenparser/tokenparser.eval.cxx
@@ -6,16 +6,56 @@ namespace Tokenparser {
 
 	extern Token c_token;
 
-        ExprPtr eval(Tokens::Type till) {
-		if(c_token.ttype == Tokens::TOK_IDENTIFIER) {
-			std::string name = c_token.name;
+	ExprPtr evalIDTF() {
+		if(eat(Tokens::TOK_INC)) {
+
+			ExprPtr idtf_expr = evalIDTF();
+
+			if(!idtf_expr) return 0;
+
+			return std::make_shared<UnaryExpression>(idtf_expr, OPE::INCB);
+		} else if(eat(Tokens::TOK_DEC)) {
+
+			ExprPtr idtf_expr = evalIDTF();
+
+			if(!idtf_expr) return 0;
+
+			return std::make_shared<UnaryExpression>(idtf_expr, OPE::DECB);
+		} else if(c_token.ttype == Tokens::TOK_IDENTIFIER) {
+			std::shared_ptr<VariableExpression> var_expr = std::make_shared<VariableExpression>(c_token.name);
 			eat(Tokens::TOK_IDENTIFIER);
-			if(eat(Tokens::TOK_ASSIGN))
-				return std::make_shared<BinaryExpression>(
-							std::make_shared<VariableExpression>(name),
-							OPE::ASSIGN,
-							eval(Tokens::TOK_SEMICOLON));
-		} else if(c_token.ttype == Tokens::TOK_NUMBER_LITERAL) {
+
+			if(eat(Tokens::TOK_INC))
+				return std::make_shared<UnaryExpression>(var_expr, OPE::INCA);
+			else if(eat(Tokens::TOK_DEC))
+				return std::make_shared<UnaryExpression>(var_expr, OPE::DECA);
+
+
+			return var_expr;
+		}
+
+		return 0;
+	}
+
+	ExprPtr eval(Tokens::Type till) {
+		{
+			ExprPtr idtf_expr = evalIDTF();
+			if(idtf_expr) {
+				if(eat(Tokens::TOK_ASSIGN))
+					return std::make_shared<BinaryExpression>(
+								idtf_expr,
+								OPE::ASSIGN,
+								eval(Tokens::TOK_SEMICOLON));
+				else {
+					std::shared_ptr c_expr = std::make_shared<ExpressionStatement>();
+					c_expr->expr = idtf_expr;
+				}
+			}
+		}
+
+
+
+		if(c_token.ttype == Tokens::TOK_NUMBER_LITERAL) {
 			std::string name = c_token.name;
 			eat(Tokens::TOK_NUMBER_LITERAL);
 
